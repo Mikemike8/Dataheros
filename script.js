fe653dbf7993436376ba54676bed091c38021150
Mikemike8 <Michaeljh811@gmail.com>
2024-05-01 00:34:03 -0500

    yeet

---
+++ script.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+const app = express();
+const API = process.env.API_KEYS
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('https://data.memphistn.gov/resource/sfvz-ctaq.json');
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+const PORT = process.env.PORT || 3006;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
